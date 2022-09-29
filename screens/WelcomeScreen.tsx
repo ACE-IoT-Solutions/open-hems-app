a3799b86b6f7eec2a22d3c06a4127b5e2e518ee2
Jason017 <jasonguan0107@gmail.com>
2022-09-29 12:38:02 -0400

    small changes on svg icon and styling for the splash page

--- screens/WelcomeScreen.tsx
+++ screens/WelcomeScreen.tsx
@@ -6,84 +6,59 @@ import {
   SafeAreaView,
 } from "react-native";
 import Phone from "../assets/svg/phone.svg";
-import Wifi from "../assets/svg/wifi.svg";
+import Welcome from "../assets/svg/welcome.svg";
 import Mail from "../assets/svg/mail.svg";
 import ArrowUp from "../assets/svg/arrow-up.svg";
 
 export function WelcomeScreen() {
   return (
     <SafeAreaView style={styles.container}>
-      {/* Title */}
       <View style={styles.titleContainer}>
-        <Text style={{ flex: 1, marginBottom: -20, fontSize: 30, marginLeft: 100, fontWeight: "bold" }}>
-          connecting
-        </Text>
-        <View style={{ flex: 1, maxWidth: 140, marginLeft: 100, flexDirection: "row", justifyContent: "space-between" }}>
-          <Wifi width={45} height={45}/>
-          <Text style={{ marginTop: -8, fontSize: 55, fontWeight: "bold" }}>
-            MHA
-          </Text>
-        </View>
+        <Welcome width={200} height={200}/>
       </View>
 
-      {/* Text */}
       <View style={styles.textContainer}>
-        {/* Welcome */}
         <Text style={{ fontSize: 35, color: "white", fontWeight: "bold" }}>
           Welcome
         </Text>
 
-        {/* Divider */}
-        <View
-          style={{
-            paddingTop: 20,
-            borderBottomColor: "white",
-            borderBottomWidth: 1
-          }}
-        />
+        <View style={styles.divider}/>
 
-        {/* Description */}
-        <View style={styles.descriptionContainer}>
-          <Text style={[styles.text, {lineHeight: 20}]}>
+        <View>
+          <Text style={styles.descriptionText}>
             This app is designed to help you manage your new smart devices, just
             click on the tab below to access the device you want to control.
           </Text>
-          <Text style={[styles.text, {paddingTop: 15, lineHeight: 20}]}>
+          <Text style={styles.descriptionText}>
             If you have any questions or concerns with your devices, or are
-            having trouble during an{" "}
+            having trouble during an 
             <Text style={{ fontWeight: "bold" }}> Energy Savings Event</Text>,
             contact us via phone or email:
           </Text>
         </View>
 
-        {/* Contact */}
         <View style={styles.contactContainer}>
-          <Text style={[styles.text, {flex: 1, fontWeight: "bold", marginBottom: -50}]}>
+          <Text style={styles.contactTitleContainer}>
             The Connecting MHA Team
           </Text>
-          <View style={{flexDirection: "column", flex: 1}}>
-            <View style={styles.rowContainer}>
-              <Phone width={30} height={30}/>
-              <Text style={[styles.text, {paddingLeft: 10, paddingTop: 5}]}>
-                (629) 256-5894
-              </Text>
-            </View>
-            
-            <View style={styles.rowContainer}>
-              <Mail width={30} height={30}/>
-              <Text style={[styles.text, {paddingLeft: 10, paddingTop: 5}]}>
-                info@connecting-mha.com
-              </Text>
-            </View>
+          <View style={styles.contactRowContainer}>
+            <Phone width={30} height={30}/>
+            <Text style={styles.subText}>
+              (629) 256-5894
+            </Text>
+          </View>
+          <View style={styles.contactRowContainer}>
+            <Mail width={30} height={30}/>
+            <Text style={styles.subText}>
+              info@connecting-mha.com
+            </Text>
           </View>
         </View>
-
         
-        {/* Swipe Gesture Button */}
-        <View style={{alignItems: "center", marginTop: 20}}>
+        <View style={styles.scrollContainer}>
           <ArrowUp width={30} height={30}/>
-          <Text style={[styles.text, {paddingTop: 20}]}>
-            Swipe up to remove this homepage
+          <Text style={[styles.text, {paddingTop: 5}]}>
+            Swipe up to continue
           </Text>
         </View>
       </View>
@@ -97,12 +72,13 @@ const styles = StyleSheet.create({
     flexDirection: "column"
   },
   titleContainer: {
-    flex: 1,
-    paddingTop: 10,
+    marginTop: -45,
+    marginBottom: -20,
+    alignItems: "center"
   },
   textContainer: {
     flex: 7,
-    marginTop: 20,
+    marginTop: -20,
     paddingTop: 30,
     paddingLeft: 40,
     paddingRight: 40,
@@ -111,13 +87,27 @@ const styles = StyleSheet.create({
     borderTopRightRadius: 20,
     backgroundColor: "rgb(32, 140, 227)"
   },
-  descriptionContainer: {
-    paddingTop: 20,
+  descriptionText: {
+    fontSize: 15,
+    color: "white",
+    paddingTop: 20, 
+    lineHeight: 20
+  },
+  divider: {
+    paddingTop: 20, 
+    borderBottomColor: "white", 
+    borderBottomWidth: 1
   },
   text: {
     fontSize: 15,
     color: "white"
   },  
+  subText: {
+    fontSize: 15,
+    paddingLeft: 10, 
+    paddingTop: 5,
+    color: "white",
+  },
   contactContainer: {
     flex: 1,
     flexDirection: "column", 
@@ -132,11 +122,21 @@ const styles = StyleSheet.create({
     borderColor: "white",
     backgroundColor: "rgba(255,255,255,0.15)"
   },
-  rowContainer: {
+  contactTitleContainer: {
+    flex: 1, 
+    fontSize: 15,
+    fontWeight: "bold", 
+    color: "white",
+    marginBottom: 5
+  },
+  contactRowContainer: {
     flex: 1,
-    maxWidth: 200, 
     marginBottom: 10,
     alignItems: "flex-start",
     flexDirection: "row", 
+  },
+  scrollContainer: {
+    alignItems: "center", 
+    marginTop: 30
   }
 });
