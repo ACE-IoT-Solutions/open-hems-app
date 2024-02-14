a9269c593720528753c41f451593bf9b34e4a762
Mia Laurea <mialaurea97@gmail.com>
2024-02-14 10:46:32 -0800

    improvement: adjust styling for auto mode w proper offsets

--- components/ThermostatScreen/ThermostatGauge.tsx
+++ components/ThermostatScreen/ThermostatGauge.tsx
@@ -15,6 +15,9 @@ import {
   PanGestureHandlerStateChangeEvent,
   State,
 } from "react-native-gesture-handler";
+import { DemandResponseStatus, ThermostatMode } from "../../types";
+import { isActiveDemandResponseStatus } from "../../utils/hems";
+import { toNumber } from "lodash";
 
 type ThermostatOnDragParams = {
   direction: "up" | "down";
@@ -24,6 +27,8 @@ type ThermostatOnDragParams = {
 type ThermostatGaugeProps = {
   label?: string;
   setPoint: number;
+  mode?: ThermostatMode;
+  drStatus: DemandResponseStatus;
   interiorTemp: number;
   onPressWarm(): void;
   onPressCool(): void;
@@ -34,7 +39,9 @@ type ThermostatGaugeProps = {
 
 export const ThermostatGauge = ({
   disabled,
+  mode,
   label,
+  drStatus,
   setPoint,
   interiorTemp,
   onPressWarm,
@@ -44,7 +51,6 @@ export const ThermostatGauge = ({
 }: ThermostatGaugeProps) => {
   const [isDraggingSetpoint, setIsDraggingSetpoint] = useState(false);
   const [dragY, setDragY] = useState(0);
-
   const maxTemp = 38;
   const minTemp = 8;
 
@@ -99,41 +105,75 @@ export const ThermostatGauge = ({
         break;
     }
   };
+  const calculateOffset = (setPoint: number, type: string, drStatus: DemandResponseStatus) => {
+    var calculatedOffset = 0;
+    var offset = 2;
+    if (drStatus === "curtailed" || drStatus === "heightened") {
+      offset = offset + 1;
+    }
+
+    if (type === "heat") {
+      calculatedOffset = toNumber(getLocalTemperature(setPoint)) - offset;
+    } else if (type === "cool") {
+      calculatedOffset = toNumber(getLocalTemperature(setPoint)) + offset;
+    }
+    return calculatedOffset;
+  };
 
   return (
     // <PanGestureHandler onGestureEvent={onGesture} onHandlerStateChange={onGestureStateChange}>
-      <View style={styles.container}>
-        <View style={styles.wrapper}>
-          {label && <Text style={styles.label}>{label}</Text>}
-          <Text style={[styles.indoorTemp, disabled && styles.disabled]}>
-            {getLocalTemperature(interiorTemp)}&deg;
-          </Text>
-          <Text style={[styles.setpoint, pendingActivity && styles.activeSetpoint, disabled && styles.disabled]}>
-            {getLocalTemperature(setPoint)}&deg;
-          </Text>
-          <View style={styles.controls}>
-            <ThermostatGaugeButton
-              label="Warm"
-              icon={<ThermostatUpIcon />}
-              disabled={disabled || setPoint >= maxTemp}
-              onPress={onPressWarm}
-            />
+    <View style={styles.container}>
+      <View style={styles.wrapper}>
+        {label && <Text style={styles.label}>{label}</Text>}
+        <Text style={[styles.indoorTemp, pendingActivity && styles.activeSetpoint, disabled && styles.disabled]}>
+          {getLocalTemperature(interiorTemp)}&deg;
+        </Text>
+        {mode === "auto" ? (
+          <View style={{ flexDirection: "row" }}>
+            <View style={styles.container}>
+              <Text style={[styles.setpointLabel, disabled && styles.disabled]}>Cool</Text>
+              <Text style={[styles.setpoint, disabled && styles.disabled]}>
+                {calculateOffset(setPoint, "cool", drStatus)}&deg;
+              </Text>
+            </View>
             <View style={styles.spacer} />
-            <ThermostatGaugeButton
-              label="Cool"
-              icon={<ThermostatDownIcon />}
-              disabled={disabled || setPoint <= minTemp}
-              onPress={onPressCool}
-            />
+            <View style={styles.spacer} />
+            <View style={styles.container}>
+              <Text style={[styles.setpointLabel, disabled && styles.disabled]}>Heat</Text>
+              <Text style={[styles.setpoint, disabled && styles.disabled]}>
+                {calculateOffset(setPoint, "heat", drStatus)}&deg;
+              </Text>
+            </View>
           </View>
-        </View>
-        <View style={styles.dialContainer}>
-          <ThermostatDial />
-          <View style={[{ position: "absolute" }, setpointDotStyle, pendingActivity && styles.activeSetpointDot]}>
-            <ThermostatSetpoint />
-          </View>
+        ) : (
+          <>
+            <Text style={[styles.setpointLabel, disabled && styles.disabled]}>{mode}</Text>
+            <Text style={[styles.setpoint, disabled && styles.disabled]}>{getLocalTemperature(setPoint)}&deg;</Text>
+          </>
+        )}
+        <View style={styles.controls}>
+          <ThermostatGaugeButton
+            label="Cool"
+            icon={<ThermostatDownIcon />}
+            disabled={disabled || setPoint <= minTemp}
+            onPress={onPressCool}
+          />
+          <View style={styles.spacer} />
+          <ThermostatGaugeButton
+            label="Warm"
+            icon={<ThermostatUpIcon />}
+            disabled={disabled || setPoint >= maxTemp}
+            onPress={onPressWarm}
+          />
         </View>
       </View>
+      <View style={styles.dialContainer}>
+        <ThermostatDial />
+        <View style={[{ position: "absolute" }, setpointDotStyle, pendingActivity && styles.activeSetpointDot]}>
+          <ThermostatSetpoint />
+        </View>
+      </View>
+    </View>
     // </PanGestureHandler>
   );
 };
@@ -153,10 +193,11 @@ const styles = StyleSheet.create({
     minWidth: 118,
   },
   label: {
-    ...typography.headline3,
+    ...typography.headline3Bold,
+    color: theme.text,
   },
-  setpoint: {
-    ...typography.label,
+  indoorTemp: {
+    ...typography.headline1,
     color: theme.primary,
   },
   activeSetpoint: {
@@ -174,8 +215,15 @@ const styles = StyleSheet.create({
   disabled: {
     color: theme.disabledText,
   },
-  indoorTemp: {
-    ...typography.headline1,
+  setpointLabel: {
+    ...typography.headline3Bold,
+    fontSize: 14,
+    textTransform: "capitalize",
+    color: theme.text,
+  },
+  setpoint: {
+    ...typography.label,
+    fontSize: 18,
     color: theme.primary,
     textAlign: "center",
     width: "100%",
@@ -185,7 +233,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     flexWrap: "wrap",
     justifyContent: "space-around",
-    marginTop: 110,
+    marginTop: 140,
     width: "100%",
   },
   spacer: {
